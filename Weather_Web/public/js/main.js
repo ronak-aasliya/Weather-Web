e3d0785a34984cd52d03da6f98a5e49d601af18f
ronak-aasliya <79149879+ronak-aasliya@users.noreply.github.com>
2023-10-27 02:44:01 +0530

    Update main.js

--- Weather_Web/public/js/main.js
+++ Weather_Web/public/js/main.js
@@ -67,7 +67,7 @@ const getInfo = async (event) => {
     datahide.classList.add('data_hide');
   } else {
     try {
-      let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=199f221bdf35cc500b974dd35c22161c`;
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=199f221bdf35cc500b974dd35c22161c`;
       const response = await fetch(url);
 
       const data = await response.json();
